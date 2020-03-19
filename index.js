70e1ab4142c3019a4469e76f960b8ca0e30f7009
Dlray89 <dlrayjr89@gmail.com>
2020-03-19 18:42:20 -0400

    model is builded

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
+
 const server = require("./server")
 
 
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => {
     console.log(`\n *** Server is running on http://localhost:${PORT} ** \n`)
--- routes/characters.model.js
+++ routes/characters.model.js
@@ -4,8 +4,32 @@ const database = require('../data/db.config')
 
 module.exports = {
     find,
+    findById,
+    add,
+    update,
+    remove
 }
 
 function find(){
     return database("characters")
+}
+
+function findById(id){
+ return database("characters")
+ .where({ id })
+ .first()
+}
+
+function add(characters){
+    return database("characters")
+    .insert(characters, "id")
+    .then(ids => ({id: ids[0]} ))
+}
+
+function update(){
+
+}
+
+function remove(){
+
 }
